e1b88764a8fd3ef8fdd6ca6556e9fa65aec20040
cloakedninjas <me@danieljackson.dev>
2025-05-26 15:39:53 +0300

    More core elements added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import './App.css'
+import diagram from './assets/placeholder-img.png';
+import haynesLogo from './assets/haynes-logo-bw.png';
 
 function App() {
     const [count, setCount] = useState(0)
@@ -12,16 +14,22 @@ function App() {
                         <h1>
                             <div className="make">Ford</div>
                             <div className="model">Capri</div>
+                            <div className="variant-info">
+                                All V6 models (including Series III)<br />
+                                1974 to 1998 ▢ 2792 cc ▢ 2994 cc
+                            </div>
                         </h1>
                     </header>
-                    <div className="logo box-section"></div>
+                    <div className="logo box-section">
+                        <img src={haynesLogo} alt="Haynes Logo"/>
+                    </div>
                 </div>
 
                 <h2 className="book-type box-section">
                     Owners Workshop Manual
                 </h2>
-                <div className="image">
-                    <img src="./assets/placeholder-img.png"/>
+                <div className="diagram box-section">
+                    <img src={diagram} alt="Diagram"/>
                 </div>
 
             </section>
