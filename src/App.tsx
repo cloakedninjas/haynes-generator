1754637c1f413ea632cf36fd937330d6d2d2a683
cloakedninjas <me@danieljackson.dev>
2025-06-09 22:15:42 +0300

    Style additions

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,62 @@
-import { useState } from 'react'
+import { type ChangeEvent, useState } from 'react'
 import './App.css'
-import diagram from './assets/placeholder-img.png';
 import haynesLogo from './assets/haynes-logo-bw.png';
-
-const make = 'Ford';
-const model = 'Capri';
-const variant = `All V6 models (including Series III)
-1974 to 1998 ▢ 2792 cc ▢ 2994 cc`;
-const bookType = 'Owners Workshop Manual';
-const img = diagram;
+import * as React from 'react';
 
 function App() {
-    const [count, setCount] = useState(0)
+    const [formData, setFormData] = useState({
+        make: 'Ford',
+        model: 'Capri',
+        variant: `All V6 models (including Series III)
+1974 to 1998 ▢ 2792 cc ▢ 2994 cc`,
+        bookType: 'Owners Workshop Manual',
+        image: 'src/assets/placeholder-img.png',
+        background: '#8f083c'
+    });
+
+    function handleInput(e: ChangeEvent<HTMLInputElement>) {
+        const {name, value} = e.target;
+        setFormData((prevState) => ({...prevState, [name]: value}));
+    }
+
+    const style = {'--background-color': formData.background} as React.CSSProperties;
 
     return (
-        <main>
+        <main style={style}>
             <section className="preview">
                 <div className="top-section">
                     <header className="box-section">
                         <h1>
-                            <div className="make">{ make }</div>
-                            <div className="model">{ model }</div>
+                            <div className="make">{formData.make}</div>
+                            <div className="model">{formData.model}</div>
                         </h1>
-                        <h2 className="variant-info">{ variant }</h2>
+                        <h2 className="variant-info">{formData.variant}</h2>
                     </header>
                     <div className="logo box-section">
                         <img src={haynesLogo} alt="Haynes Logo"/>
                     </div>
                 </div>
 
-                <h2 className="book-type box-section">{ bookType }</h2>
-                <div className="diagram box-section" style={{backgroundImage: `url(${img}`}}>
-                </div>
+                <h2 className="book-type box-section">{formData.bookType}</h2>
+                <div className="diagram box-section" style={{backgroundImage: `url(${formData.image})`}}></div>
 
             </section>
-            {/*<form className="controls">
-                Controls
-            </form>*/}
+            <form className="controls">
+                {
+                    Object.keys(formData).map((key) => (
+                        <p key={key}>
+                            <label htmlFor={key}>{key}</label>
+                            <input
+                                type="text"
+                                id={key}
+                                name={key}
+                                value={formData[key as keyof typeof formData]}
+                                onInput={handleInput}
+                            />
+                        </p>
+                    ))
+                }
+            </form>
         </main>
     )
 }
