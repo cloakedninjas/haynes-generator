6a93ee63aec7d55860153fbc01e15f79db0be407
cloakedninjas <me@danieljackson.dev>
2025-07-29 21:09:23 +0300

    Fix URLs for assets

--- src/lib/presets.ts
+++ src/lib/presets.ts
@@ -1,5 +1,7 @@
 import type { CoverFields } from '../App.tsx';
 
+const publicBase = '/haynes-generator';
+
 export const swatches: string[] = [
     '#8f083c',
     '#cc0449',
@@ -20,35 +22,35 @@ export const presets: CoverFields[] = [
         variant: `All V6 models (including Series III)
 1974 to 1998 ▢ 2792 cc ▢ 2994 cc`,
         bookType: "Owners Workshop Manual",
-        image: "/ford-capri.png",
+        image: `${publicBase}/ford-capri.png`,
         background: swatches[0]
     }, {
         make: "Morris",
         model: "Minor 1000",
         variant: "All models (1956 to 1971)\n▢ 948 cc ▢ 1098 cc",
         bookType: "Owners Workshop Manual",
-        image: "/morris-minor-1000.png",
+        image: `${publicBase}/morris-minor-1000.png`,
         background: swatches[7]
     }, {
         make: "U.S.S.",
         model: "Enterprise",
         variant: "2151 onwards\n▢ NCC-1701-A to NCC-1701-D",
         bookType: "Owners Workshop Manual",
-        image: "/uss-enterprise.png",
+        image: `${publicBase}/uss-enterprise.png`,
         background: swatches[6]
     }, {
         make: "Lockheed Martin",
         model: "F22 Raptor",
         variant: "F/A-22▢ Lightning II",
         bookType: "Owners Workshop Manual",
-        image: "/f22.png",
+        image: `${publicBase}/f22.png`,
         background: swatches[6]
     }, {
         make: "Bernese",
         model: "Mountain Dog",
         variant: "Tri-color boofer",
         bookType: "Owners Kennel Manual",
-        image: "/bernese.png",
+        image: `${publicBase}/bernese.png`,
         background: swatches[2]
     }
 ];
